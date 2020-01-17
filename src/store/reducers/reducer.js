2e527bd7ad54b77b19dfefb43484bb882f7a3d6c
Baytik Tumanbaev <jumka.wows@gmail.com>
2020-01-17 18:46:19 +0600

    Закончил

--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -8,16 +8,6 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
         case FETCH_PRODUCTS_SUCCESS:
             return {...state, products: action.products};
-        /*case REMOVE_PRODUCT:
-            if (state.products[action.productName] === 0) {
-                return state;
-            }
-            return {...state,
-                products: {
-                    ...state.products,
-                    [action.productName]: state.products[action.productName] - 1,
-                },
-            };*/
         default:
             return state;
     }
